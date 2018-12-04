b59ca49674806e9a1dc1a0172cf06700d6811e2a
Gabriel Caniglia <gcaniglia1@gmail.com>
2018-12-04 10:43:06 -0600

    Modal link code

--- site.js
+++ site.js
@@ -29,9 +29,7 @@ $(document).ready(function() {
 });
 */
 
-if (window.location.hash == "#marathon") {
-     $('#marathonModal').modal('show');
-}
+
 
 //text carousel code
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
